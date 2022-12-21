09ff3acef76079c3834b6ee735b738bfe469a9a0
Yousuf Ali <aliyusuf714@gmail.com>
2022-12-21 22:28:44 +0000

    add Iam create 2 users

--- iam.tf
+++ iam.tf
@@ -1,3 +1,10 @@
-resource "aws_iam_user" "nuur" {
-  name = "test-user"
+resource "random_integer" "random" {
+    max= 4
+    min= 2
+  byte_length = 8
 }
+ resource "aws_iam_user" "Ahmed" {
+    count = 2
+   name = "T-Ahmed-${random_integer.random.id}"
+}
+  
