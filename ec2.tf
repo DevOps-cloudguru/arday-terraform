6d3800bc5d9f5511031f8f60d64349cd6ece4613
Mohamed Roble <mohamed.roble@bjss.com>
2023-01-03 19:32:18 +0000

    first Windows OS with SSH access 2
    this is for place holder, after a review and discussion it will be merged to the main

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,45 @@
 ### ec2 instance 
-resource "aws_instance" "arday_ec2" {
-  ami           = "ami-0beaa649c482330f7"
-  instance_type = "t3.micro"
-  key_name = "sandbox"
-  vpc_security_group_ids =  [aws_security_group.arday_sg.id]
 
-   root_block_device {
-    volume_size = 16
+# Get the latest Windows Server 2022 AMI
+data "aws_ami" "windows_server" {
+  most_recent = true
+  owners      = ["801119661308"]
+  # owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2022-English-Full-Base*"]
+  }
+}
+
+# Get latest Ubuntu Server
+data "aws_ami" "ubuntu_server" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "arday_ec2" {
+  ami = "ami-0beaa649c482330f7"
+  # ami           = data.aws_ami.ubuntu_server.id
+  instance_type = "t3.micro"
+  key_name      = "sandbox"
+  # subnet_id = aws_subnet.main-public.id
+  vpc_security_group_ids = [aws_security_group.arday_sg.id]
+  # get_password_data = true
+
+  # user_data = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 30
   }
   tags {
-    Name = "machine-server"
+    Name = "Ubuntu-server"
+    # Name = "Windows-server"
   }
 
 }
--- provider.tf
+++ provider.tf
@@ -1,11 +1,49 @@
+### different ways to setup backends/providers
+
+#### to use external/cloud backend is more secure and reliable 
+
+# for S3 backend of our infrastructure
 terraform {
   backend "s3" {
-    bucket   = "terraform-cloudguru-hh-test"
-    key      = "terraform-cloudgurur.tfstate"
+    bucket         = "terraform-cloudguru-hh-test"
+    key            = "terraform-cloudgurur.tfstate"
     dynamodb_table = "terarform.lock"
-    region   = "us-east-2"
+    region         = "us-east-2"
   }
 }
 
+##### for local providers use
+#############################
+# to provide configuration that terraform need to know
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+# Provider block, provides the information need to access AWS specificly
+provider "aws" {
+  region = "eu-north-1"
+  # region                   = "us-west-2"
+  shared_credentials_files = [("~/.aws/credentials")]
+  # shared_credentials_file = "/Users/M ROBLE/.aws/credentials"
+  profile = "devenv01"
+  # profile = "arday1"
+}
+# a provider is to interact with the API of your infrastructure
 
 
+##### for HashiCorp terraform cloud backend is easy to manage 
+terraform {
+  cloud {
+    organization = "ROBLE-Engineering"
+
+    workspaces {
+      name = "arday-backend"
+    }
+  }
+}
+# requires to create token and login from the terminal 
+# to create token, click your avator and scroll then create new token
+# terraform login will be the command to login then paste your token
